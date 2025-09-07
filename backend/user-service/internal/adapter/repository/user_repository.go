df6fd30498d3d11771af7c9190c85c8c69744d43
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-09-07 22:11:18 +0700

    add user account creation functionality with email verification token generation; implement database migration for verification tokens

--- backend/user-service/internal/adapter/repository/user_repository.go
+++ backend/user-service/internal/adapter/repository/user_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"errors"
+	"time"
 	"user-service/internal/core/domain/entity"
 	"user-service/internal/core/domain/model"
 
@@ -12,10 +13,40 @@ import (
 
 type UserRepositoryInterface interface {
 	GetUserByEmail(ctx context.Context, email string) (*entity.UserEntity, error)
+	CreateUserAccount(ctx context.Context, req entity.UserEntity) error
 }
 
 type userRepository struct {
 	db *gorm.DB
+}
+
+// CreateUserAccount implements UserRepositoryInterface.
+func (u *userRepository) CreateUserAccount(ctx context.Context, req entity.UserEntity) error {
+	modelUser := model.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	if err := u.db.Create(&modelUser).Error; err != nil {
+		log.Errorf("[UserRepository-1] CreateUserAccount: %v", err)
+		return err
+	}
+
+	currentTime := time.Now()
+	modelVerify := model.VerificationToken{
+		UserID:    modelUser.ID,
+		Token:     req.Token,
+		TokenType: "email_verification",
+		ExpiresAt: currentTime.Add(time.Hour * 1),
+	}
+
+	if err := u.db.Create(&modelVerify).Error; err != nil {
+		log.Errorf("[UserRepository-2] CreateUserAccount: %v", err)
+		return err
+	}
+
+	return nil
 }
 
 func (u *userRepository) GetUserByEmail(ctx context.Context, email string) (*entity.UserEntity, error) {
