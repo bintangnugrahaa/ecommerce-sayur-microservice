f8ea4a897a584354c43ef1ec686bb274dddffe1f
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-10-05 01:28:24 +0700

    add category and product models for product service
    * created category and product structs with necessary fields and gorm annotations.
    * established relationships between products and categories using foreign keys.
    * included timestamps for created, updated, and deleted records in both models.

---
+++ backend/product-service/internal/core/domain/model/category_model.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+)
+
+type Category struct {
+	ID          int64      `gorm:"primaryKey"`
+	ParentID    *int64     `gorm:"column:parent_id"`
+	Name        string     `gorm:"column:name;not null"`
+	Icon        string     `gorm:"column:icon;not null"`
+	Status      bool       `gorm:"column:status;default:true"`
+	Slug        string     `gorm:"column:slug;unique"`
+	Description string     `gorm:"column:description"`
+	CreatedAt   time.Time  `gorm:"column:created_at;default:CURRENT_TIMESTAMP"`
+	UpdatedAt   *time.Time `gorm:"column:updated_at"`
+	DeletedAt   *time.Time `gorm:"column:deleted_at;index"`
+	Products    []Product  `gorm:"foreignKey:CategorySlug;references:Slug"`
+}
