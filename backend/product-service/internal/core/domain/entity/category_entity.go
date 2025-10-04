f2096a6c35a307147812fff536b29fea4e41bf1a
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-10-05 01:32:20 +0700

    add category and product entity definitions
    * created categoryentity and productentity structs with relevant fields for the product service.
    * established relationships between products and categories through the categoryslug field.
    * included necessary json annotations for serialization.

---
+++ backend/product-service/internal/core/domain/entity/category_entity.go
@@ -0,0 +1,12 @@
+package entity
+
+type CategoryEntity struct {
+	ID          int64           `json:"id"`
+	ParentID    *int64          `json:"parent_id"`
+	Name        string          `json:"name"`
+	Icon        string          `json:"icon"`
+	Status      string          `json:"status"`
+	Slug        string          `json:"slug"`
+	Description string          `json:"description"`
+	Products    []ProductEntity `json:"products"`
+}
