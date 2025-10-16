3f7b026a301181b16ad5da93531c3ecd475a3ecf
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-10-16 08:57:23 +0700

    feat(product): add GetByID method in ProductRepository for detailed product retrieval
    Implement GetByID method to fetch product by ID with related child entities
    and category details. Improve error handling for record-not-found scenarios.

--- backend/product-service/internal/adapter/repository/produck_repository.go
+++ backend/product-service/internal/adapter/repository/produck_repository.go
@@ -14,10 +14,72 @@ import (
 
 type ProductRepositoryInterface interface {
 	GetAll(ctx context.Context, query entity.QueryStringProduct) ([]entity.ProductEntity, int64, int64, error)
+	GetByID(ctx context.Context, productID int64) (*entity.ProductEntity, error)
 }
 
 type productRepository struct {
 	db *gorm.DB
+}
+
+// GetByID implements ProductRepositoryInterface.
+func (p *productRepository) GetByID(ctx context.Context, productID int64) (*entity.ProductEntity, error) {
+	modelProduct := model.Product{}
+
+	if err := p.db.WithContext(ctx).Preload("Category").First(&modelProduct, "id = ?", productID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			err = errors.New("404")
+		}
+		log.Errorf("[ProductRepository-1] GetByID: %v", err)
+		return nil, err
+	}
+
+	modelParent := []model.Product{}
+	err := p.db.WithContext(ctx).Preload("Category").Where("parent_id = ?", modelProduct.ID).Find(&modelParent).Error
+	if err != nil {
+		log.Errorf("[ProductRepository-2] GetByID: %v", err)
+		return nil, err
+	}
+
+	childEntities := []entity.ProductEntity{}
+	for _, val := range modelParent {
+		childEntities = append(childEntities, entity.ProductEntity{
+			ID:           val.ID,
+			CategorySlug: val.CategorySlug,
+			ParentID:     val.ParentID,
+			Name:         val.Name,
+			Image:        val.Image,
+			Description:  val.Description,
+			RegulerPrice: val.RegulerPrice,
+			SalePrice:    val.SalePrice,
+			Unit:         val.Unit,
+			Weight:       val.Weight,
+			Stock:        val.Stock,
+			Variant:      val.Variant,
+			Status:       val.Status,
+			CategoryName: val.Category.Name,
+			Child:        childEntities,
+			CreatedAt:    val.CreatedAt,
+		})
+	}
+
+	return &entity.ProductEntity{
+		ID:           modelProduct.ID,
+		CategorySlug: modelProduct.CategorySlug,
+		ParentID:     modelProduct.ParentID,
+		Name:         modelProduct.Name,
+		Image:        modelProduct.Image,
+		Description:  modelProduct.Description,
+		RegulerPrice: modelProduct.RegulerPrice,
+		SalePrice:    modelProduct.SalePrice,
+		Unit:         modelProduct.Unit,
+		Weight:       modelProduct.Weight,
+		Stock:        modelProduct.Stock,
+		Variant:      modelProduct.Variant,
+		Status:       modelProduct.Status,
+		CategoryName: modelProduct.Category.Name,
+		Child:        childEntities,
+		CreatedAt:    modelProduct.CreatedAt,
+	}, nil
 }
 
 // GetAll implements ProductRepositoryInterface.
