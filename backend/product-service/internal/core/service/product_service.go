6b9abbc3ec7219d6c6538e639d39475ab1cc51c6
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-10-17 17:12:08 +0700

    feat(product): implement ProductService with CRUD operations
    Add ProductServiceInterface and productService struct to handle product-related logic.
    Implement Create, GetAll, GetByID, Update, and Delete methods delegating functionality
    to ProductRepositoryInterface. Establish foundational structure for product management
    within the service layer.

---
+++ backend/product-service/internal/core/service/product_service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"context"
+	"product-service/internal/adapter/repository"
+	"product-service/internal/core/domain/entity"
+)
+
+type ProductServiceInterface interface {
+	GetAll(ctx context.Context, query entity.QueryStringProduct) ([]entity.ProductEntity, int64, int64, error)
+	GetByID(ctx context.Context, productID int64) (*entity.ProductEntity, error)
+	Create(ctx context.Context, req entity.ProductEntity) error
+	Update(ctx context.Context, req entity.ProductEntity) error
+	Delete(ctx context.Context, productID int64) error
+}
+
+type productService struct {
+	repo repository.ProductRepositoryInterface
+}
+
+// Create implements ProductServiceInterface.
+func (p *productService) Create(ctx context.Context, req entity.ProductEntity) error {
+	return p.repo.Create(ctx, req)
+}
+
+// Delete implements ProductServiceInterface.
+func (p *productService) Delete(ctx context.Context, productID int64) error {
+	return p.repo.Delete(ctx, productID)
+}
+
+// GetAll implements ProductServiceInterface.
+func (p *productService) GetAll(ctx context.Context, query entity.QueryStringProduct) ([]entity.ProductEntity, int64, int64, error) {
+	return p.repo.GetAll(ctx, query)
+}
+
+// GetByID implements ProductServiceInterface.
+func (p *productService) GetByID(ctx context.Context, productID int64) (*entity.ProductEntity, error) {
+	return p.repo.GetByID(ctx, productID)
+}
+
+// Update implements ProductServiceInterface.
+func (p *productService) Update(ctx context.Context, req entity.ProductEntity) error {
+	return p.repo.Update(ctx, req)
+}
+
+func NewProductService(repo repository.ProductRepositoryInterface) ProductServiceInterface {
+	return &productService{repo: repo}
+}
