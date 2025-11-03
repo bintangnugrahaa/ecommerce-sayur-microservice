7cc49f2904e83438e32d6234c3c0876a00dae8d4
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-11-03 17:26:52 +0700

    feat(order-service): initialize order service with configuration, command structure, and database connections

---
+++ order-service/config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import "github.com/spf13/viper"
+
+type App struct {
+	AppPort string `json:"app_port"`
+	AppEnv  string `json:"app_env"`
+
+	JwtSecretKey string `json:"jwt_secret_key"`
+}
+
+type PsqlDB struct {
+	Host      string `json:"host"`
+	Port      string `json:"port"`
+	User      string `json:"user"`
+	Password  string `json:"password"`
+	DBName    string `json:"db_name"`
+	DBMaxOpen int    `json:"db_max_open"`
+	DBMaxIdle int    `json:"db_max_idle"`
+}
+
+type RabbitMQ struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
+type Redis struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+}
+
+type Config struct {
+	App           App           `json:"app"`
+	Psql          PsqlDB        `json:"psql"`
+	RabbitMQ      RabbitMQ      `json:"rabbitmq"`
+	Redis         Redis         `json:"redis"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		App: App{
+			AppPort: viper.GetString("APP_PORT"),
+			AppEnv:  viper.GetString("APP_ENV"),
+
+			JwtSecretKey: viper.GetString("JWT_SECRET_KEY"),
+		},
+		Psql: PsqlDB{
+			Host:      viper.GetString("DATABASE_HOST"),
+			Port:      viper.GetString("DATABASE_PORT"),
+			User:      viper.GetString("DATABASE_USER"),
+			Password:  viper.GetString("DATABASE_PASSWORD"),
+			DBName:    viper.GetString("DATABASE_NAME"),
+			DBMaxOpen: viper.GetInt("DATABASE_MAX_OPEN_CONNECTION"),
+			DBMaxIdle: viper.GetInt("DATABASE_MAX_IDLE_CONNECTION"),
+		},
+		RabbitMQ: RabbitMQ{
+			Host:     viper.GetString("RABBITMQ_HOST"),
+			Port:     viper.GetString("RABBITMQ_PORT"),
+			User:     viper.GetString("RABBITMQ_USER"),
+			Password: viper.GetString("RABBITMQ_PASSWORD"),
+		},
+		Redis: Redis{
+			Host: viper.GetString("REDIS_HOST"),
+			Port: viper.GetString("REDIS_PORT"),
+		},
+	}
+}
